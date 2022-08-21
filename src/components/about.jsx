510755716b5591f00e884c69c1d36e706284e2ec
Gabriel Stoklas <xstoklas@stuba.sk>
2022-08-21 22:50:33 +0200

    added new texts and some small changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,19 +11,34 @@ export const About = (props) => {
             <div className="about-text">
               <h2>{props.data ? props.data.AboutUs : "loading..."}</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
+              <h2>{props.data ? props.data.ConsHeader : "loading..."}</h2>
+              <div className="list-styleCons">
+                <div className="col-lg-12 col-sm-12 col-xs-12">
+                  <ul>
+                    <li>
+                      <b>{props.data ? props.data.Cons1Header : "loading..."}</b> - {props.data ? props.data.Cons1 : "loading..."}
+                    </li>
+                    <li>
+                      {props.data ? props.data.Cons2 : "loading..."}
+                    </li>
+                    <li>
+                      <b>{props.data ? props.data.Cons3Header : "loading..."}</b> - {props.data ? props.data.Cons3 : "loading..."}
+                    </li>
+                    <li>
+                      <b>{props.data ? props.data.Cons4Header : "loading..."}</b> - {props.data ? props.data.Cons4 : "loading..."}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <h2>{props.data ? props.data.WhyHeader : "loading..."}</h2>
+              <div className="list-styleWhy">
+                <div className="col-lg-12 col-sm-12 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why.map((d, i) => (
                           <li key={`${d}-${i}`}>{d}</li>
                         ))
                       : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
                     {props.data
                       ? props.data.Why2.map((d, i) => (
                           <li key={`${d}-${i}`}> {d}</li>
@@ -31,6 +46,15 @@ export const About = (props) => {
                       : "loading"}
                   </ul>
                 </div>
+                {/* <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div> */}
               </div>
             </div>
           </div>
